ee02c2586835ac8e66926566a1b9a0a5cbbd8ea5
Jacob Pratt <the.z.cuber@gmail.com>
2017-07-12 18:18:44 -0400

    Commenting code, slight changes for readability

--- helper_functions.rb
+++ helper_functions.rb
@@ -1,5 +1,6 @@
 require 'erb'
 
+# for swapping in events if it wasn't caught by JS client-side
 $message_symbols = {
   ':music:' => '♫',
   ':rocket:' => '🚀',
@@ -7,11 +8,13 @@ $message_symbols = {
   ':satellite:' => '🛰'
 }
 
+# take a file name, return rendered HTML from .erb file
 def render_erb fname
   file = File.read("#{fname}.erb")
   ERB.new(file).result(binding)
 end
 
+# fully formatted markdown post
 def reddit_post
   if session[:events]
     "#{session[:intro]}\n\n#{session[:viewing]}\n\n### Live Updates\n#{format_events session[:events]}\n\n#{session[:stats]}\n\n#{session[:mission]}\n\n#{session[:landing]}\n\n#{session[:resources]}\n\n#{session[:participate]}"
@@ -20,6 +23,7 @@ def reddit_post
   end
 end
 
+# get score, number of comments, and html from reddit post id
 def post_info id
   submission = request.env['redd.session'].from_ids ["t3_#{id}"].to_ary
   {
@@ -29,6 +33,8 @@ def post_info id
   }
 end
 
+# create a self post with a given title and text
+# specified subreddit can be swapped out easily
 def make_post title, text=''
   request
     .env['redd.session']
@@ -36,9 +42,11 @@ def make_post title, text=''
     .submit title, text: text, sendreplies: false
 end
 
+# creates a post if it doesn't exist
+# edits post if it exists
 def update_post
   if session[:post] == nil
-    post = make_post 'Testing automated post creation', reddit_post
+    post = make_post 'Testing automated post creation', reddit_post  # still need to allow runtime title creation
     session[:post] = post.id
   else
     post = request.env['redd.session'].from_ids ["t3_#{session[:post]}"]
@@ -47,6 +55,7 @@ def update_post
   post_info(session[:post])['html']
 end
 
+# get list of events and return a formatted table
 def format_events events
   if events != nil
     str = "| Time | Update |\n| --- | --- |"
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,6 +1,6 @@
 'use strict';  // don't allow crap JS practices
 
-// use vanilla JS over jQuery where feasible
+// use vanilla JS over jQuery where it doesn't take a terrible amount of effort
 // only thing I'm really using jQuery for is AJAX and jQuery UI
 
 window.onload = () => {
@@ -42,6 +42,7 @@ window.onload = () => {
 	setInterval(updateCountdown, 1000);
 }
 
+// save a certain section to reddit
 function save() {
 	const elem = arguments[0].element;
 
@@ -50,7 +51,7 @@ function save() {
 
 	$.ajax({
 		method: 'POST',
-		url: '/update',
+		url: 'update',
 		data: { id: id, value: value },
 		success: data => {
 			$('#' + id + ' + .editor-toolbar > a[title="Save to reddit"]').addClass('highlight');
@@ -59,6 +60,8 @@ function save() {
 	});
 }
 
+// save events section
+// handled differently because it needs parsing
 function saveEvents() {
 	const events = document.getElementById('events').children;
 
@@ -81,6 +84,7 @@ function saveEvents() {
 	});
 }
 
+// gets status page and updates section
 function updateStats() {
 	$.ajax({
 		url: 'status',
@@ -88,37 +92,46 @@ function updateStats() {
 	});
 }
 
+// add an event at the top of the list
 function addEvent() {
 	const events = document.getElementById('events');
 
 	const row = document.createElement('li');
 
+	// add "sortable" icon handle
 	const icon = document.createElement('i');
 	icon.classList.add('sort-icon', 'ui-sortable-handle');
 	row.appendChild(icon);
 
+	// T± clickable
 	const tPM = document.createElement('span');
 	tPM.setAttribute('onclick', 'this.innerHTML = this.innerHTML == "T+" ? "T-" : "T+"');
 	tPM.innerHTML = 'T+';
 	row.appendChild(tPM);
 
+	// T± input and following text
 	row.appendChild( document.createElement('input') );
 	row.innerHTML += ' Message: ';
 
+	// message input
 	const message = document.createElement('input');
 	message.setAttribute('onkeyup', 'hotSwap(this); saveIfEnter(event);');
 	row.appendChild(message);
 
+	// add new row at beginning
 	events.insertBefore(row, events.firstChild);
 }
 
+// remove event at top of list
 function removeEvent() {
 	const events = document.getElementById('events');
-	if(events.children.length > 1)
+	if(events.children.length > 1)  // don't allow removing last event
 		events.removeChild(events.firstElementChild);
 }
 
 
+// simple hash with values to swap out
+// if modifying ─ keep in mind these do not get escaped before passing to regex
 const hotSwapVals = {
 	':music:': '♫',
 	':rocket:': '🚀',
@@ -126,6 +139,7 @@ const hotSwapVals = {
 	':satellite:': '🛰'
 }
 
+// swap out text with emoji on an input
 function hotSwap(obj) {
 	if(obj.constructor !== HTMLInputElement) throw 'Object must be HTMLInputElement';
 
@@ -134,11 +148,14 @@ function hotSwap(obj) {
 	obj.value = val;
 }
 
+// bound on event inputs
 function saveIfEnter(evnt) {
 	if(evnt.keyCode == 13)  // enter
 		saveEvents()
 }
 
+// updates the countdown timer based on launch time
+// runs every second
 function updateCountdown() {
 	function pad(num) { return num < 10 ? '0'+num : num; }
 
@@ -160,26 +177,24 @@ function updateCountdown() {
 	const mins = Math.abs(launchTime - curTime) % 3600000 / 60000 | 0;
 	const secs = Math.abs(launchTime - curTime) % 60000 / 1000 | 0;
 
-	let time;
+	let time;  // placed here for scope
 
-	if(hours > 0) {
+	if(hours > 0)
 		time = hours + ':' + pad(mins) + ':' + pad(secs);
-	}
-	else if(mins > 0) {
+	else if(mins > 0)
 		time = mins + ':' + pad(secs);
-	}
-	else {
+	else
 		time = secs;
-	}
 
 	document.getElementById('timer').innerHTML = 'T' + sign + time;
 }
 
+// popup to ask for launch time
+// will be replaced by <dialog> element in future (has custom styling and looks better)
 function setLaunchTime() {
 	window.launchTime = Date.parse(prompt('Launch time: (your location, YYYY-MM-DD HH:MM:SS)'));
-	if(isNaN(window.launchTime)) {
+	if(isNaN(window.launchTime))
 		window.launchTime = null;
-	}
 	$.ajax({
 		method: 'POST',
 		url: 'update',
